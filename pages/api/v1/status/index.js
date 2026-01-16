59af98f11f602a4ea7c953fc418f6e1314695f51
Cristian Santos Felipe <43704271+Cristian695@users.noreply.github.com>
2026-01-16 23:13:33 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da media" });
+  const updatedAt = new Date().toISOString();
+
+  const getPostgresVer = await database.query("SHOW server_version;");
+  const postgresVer = getPostgresVer.rows[0].server_version;
+
+  const getMaxConnections = await database.query("SHOW max_connections;");
+  const maxConections = getMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const getOpenConnections = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const openedConnections = getOpenConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_ver: postgresVer,
+        max_connections: parseInt(maxConections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const postgresVer = await responseBody.dependencies.database.postgres_ver;
+  expect(postgresVer).toEqual("16.0");
+
+  const maxConections =
+    await responseBody.dependencies.database.max_connections;
+  expect(maxConections).toEqual(100);
+
+  const currentConections =
+    await responseBody.dependencies.database.opened_connections;
+  expect(currentConections).toBe(1);
 });
